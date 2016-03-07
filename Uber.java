4abd3de4e7e2261c67c1d0ff91082a6329cbed80
Kumar Shreyansh <g5kumars@cdf.toronto.edu>
2016-03-06 21:45:48 -0500

    finished first part

---
+++ Uber.java
@@ -0,0 +1,62 @@
+import java.sql.*;
+import java.io.*;
+import java.util.Scanner;
+class Uber {
+
+    public static void main(String args[]) throws IOException {
+        String url;
+        Connection conn;
+        PreparedStatement pStatement;
+        ResultSet rs;
+        String queryString;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Failed to find the JDBC driver");
+        }
+
+        try {
+            // In this try block, you should
+            // 1) Connect to the database and set the search path
+            // 2) Prompt the user for the name of a client
+            // 3) Make the appropriate database query string
+            //    (use the '?' placeholder!!)
+            // 4) Output the results
+            // 5) Close the connection
+
+            // NOTE: We didn't cover this in lecture, but you'll need to
+            // execute a "SET search_path TO uber" statement before
+            // executing any queries. Use the following code snippet to do so:
+            //
+            // queryString = "SET search_path TO uber";
+            // pStatement = conn.prepareStatement(queryString);
+            // pStatement.execute();
+
+            // YOUR CODE GOES HERE
+	    
+	    url = "jdbc:postgresql://localhost:5432/csc343h-g5kumars";
+	    conn = DriverManager.getConnection(url, "g5kumars", "");
+	    
+	    //read input
+	    Scanner reader = new Scanner(System.in);
+	    System.out.println("Enter a Client name:");
+	    String name = reader.nextLine();
+	    
+	    //System.out.println("Client name: " + name);
+	    
+	    queryString = "select firstname from dispatch natural join driver where request_id in (select request_id from client natural join request where firstname='" + name + "');";
+	    pStatement = conn.prepareStatement(queryString);
+	    rs = pStatement.executeQuery();
+	    
+	    while(rs.next()){
+		System.out.println(rs.getString("firstname"));
+	    }
+        }
+        catch (SQLException se) {
+            System.err.println("SQL Exception." +
+                    "<Message>: " + se.getMessage());
+        }
+    }
+}
---
+++ d
@@ -0,0 +1,12 @@
+ client_id | surname | firstname |          email          | request_id |      datetime       |            source             |          destination          
+-----------+---------+-----------+-------------------------+------------+---------------------+-------------------------------+-------------------------------
+        99 | Mason   | Daisy     | daisy@kitchen.com       |          1 | 2016-01-08 04:10:00 | eaton centre                  | pearson international airport
+       100 | Crawley | Violet    | dowager@dower-house.org |          2 | 2013-02-01 08:00:00 | dower house                   | highclere castle
+       100 | Crawley | Violet    | dowager@dower-house.org |          3 | 2013-02-02 08:00:00 | dower house                   | highclere castle
+       100 | Crawley | Violet    | dowager@dower-house.org |          4 | 2013-02-03 08:00:00 | highclere castle              | dower house
+       100 | Crawley | Violet    | dowager@dower-house.org |          5 | 2014-07-01 08:00:00 | dower house                   | pearson international airport
+       100 | Crawley | Violet    | dowager@dower-house.org |          6 | 2014-07-02 08:00:00 | pearson international airport | eaton centre
+       100 | Crawley | Violet    | dowager@dower-house.org |          7 | 2014-07-03 08:00:00 | eaton centre                  | cn tower
+       100 | Crawley | Violet    | dowager@dower-house.org |          8 | 2015-07-01 08:00:00 | cn tower                      | pearson international airport
+(8 rows)
+
